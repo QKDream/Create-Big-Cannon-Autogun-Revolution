4383d5be23c12a9eb6e4503e0d5ced6d93f649f0
QKDream <qkdream@users.noreply.github.com>
2026-08-08 13:52:20 +0800

    Fix fuze: Cartridge extends Item + FuzedItemMunition, bypasses AutocannonCartridgeItem projectile stack check

--- src/main/java/com/cbcaddon/addon/item/APFSDSAutocannonCartridgeItem.java
+++ src/main/java/com/cbcaddon/addon/item/APFSDSAutocannonCartridgeItem.java
@@ -3,15 +3,12 @@ package com.cbcaddon.addon.item;
 import com.cbcaddon.addon.entity.APFSDSAutocannonProjectile;
 import com.cbcaddon.addon.init.ModEntities;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import rbasamoyai.createbigcannons.munitions.autocannon.AbstractAutocannonProjectile;
-import rbasamoyai.createbigcannons.munitions.autocannon.AutocannonAmmoItem;
-import rbasamoyai.createbigcannons.munitions.autocannon.AutocannonAmmoType;
-import rbasamoyai.createbigcannons.munitions.autocannon.config.AutocannonProjectilePropertiesComponent;
+import rbasamoyai.createbigcannons.munitions.autocannon.AutocannonCartridgeItem;
 
-public class APFSDSAutocannonCartridgeItem extends Item implements AutocannonAmmoItem {
+public class APFSDSAutocannonCartridgeItem extends AutocannonCartridgeItem {
     public APFSDSAutocannonCartridgeItem(Properties properties) {
         super(properties);
     }
@@ -24,29 +21,5 @@ public class APFSDSAutocannonCartridgeItem extends Item implements AutocannonAmm
     @Override
     public AbstractAutocannonProjectile getAutocannonProjectile(ItemStack stack, Level level) {
         return new APFSDSAutocannonProjectile(ModEntities.APFSDS_AUTOCANNON.get(), level);
-    }
-
-    @Override
-    public AutocannonProjectilePropertiesComponent getAutocannonProperties(ItemStack stack) {
-        return AutocannonProjectilePropertiesComponent.DEFAULT;
-    }
-
-    @Override
-    public ItemStack getSpentItem(ItemStack stack) {
-        return ItemStack.EMPTY;
-    }
-
-    @Override
-    public AutocannonAmmoType getType() {
-        return AutocannonAmmoType.AUTOCANNON;
-    }
-
-    @Override
-    public boolean isTracer(ItemStack stack) {
-        return false;
-    }
-
-    @Override
-    public void setTracer(ItemStack stack, boolean tracer) {
     }
 }
--- src/main/java/com/cbcaddon/addon/item/APHEAutocannonCartridgeItem.java
+++ src/main/java/com/cbcaddon/addon/item/APHEAutocannonCartridgeItem.java
@@ -3,16 +3,13 @@ package com.cbcaddon.addon.item;
 import com.cbcaddon.addon.entity.APHEAutocannonProjectile;
 import com.cbcaddon.addon.init.ModEntities;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import rbasamoyai.createbigcannons.munitions.FuzedItemMunition;
 import rbasamoyai.createbigcannons.munitions.autocannon.AbstractAutocannonProjectile;
-import rbasamoyai.createbigcannons.munitions.autocannon.AutocannonAmmoItem;
-import rbasamoyai.createbigcannons.munitions.autocannon.AutocannonAmmoType;
-import rbasamoyai.createbigcannons.munitions.autocannon.config.AutocannonProjectilePropertiesComponent;
+import rbasamoyai.createbigcannons.munitions.autocannon.AutocannonCartridgeItem;
 
-public class APHEAutocannonCartridgeItem extends Item implements AutocannonAmmoItem, FuzedItemMunition {
+public class APHEAutocannonCartridgeItem extends AutocannonCartridgeItem implements FuzedItemMunition {
     public APHEAutocannonCartridgeItem(Properties properties) {
         super(properties);
     }
@@ -25,29 +22,5 @@ public class APHEAutocannonCartridgeItem extends Item implements AutocannonAmmoI
     @Override
     public AbstractAutocannonProjectile getAutocannonProjectile(ItemStack stack, Level level) {
         return new APHEAutocannonProjectile(ModEntities.APHE_AUTOCANNON.get(), level);
-    }
-
-    @Override
-    public AutocannonProjectilePropertiesComponent getAutocannonProperties(ItemStack stack) {
-        return AutocannonProjectilePropertiesComponent.DEFAULT;
-    }
-
-    @Override
-    public ItemStack getSpentItem(ItemStack stack) {
-        return ItemStack.EMPTY;
-    }
-
-    @Override
-    public AutocannonAmmoType getType() {
-        return AutocannonAmmoType.AUTOCANNON;
-    }
-
-    @Override
-    public boolean isTracer(ItemStack stack) {
-        return false;
-    }
-
-    @Override
-    public void setTracer(ItemStack stack, boolean tracer) {
     }
 }
