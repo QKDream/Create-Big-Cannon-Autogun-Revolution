f467ed7dfdf11d6bde558be0bac8180bc8d15cc8
QKDream <qkdream@users.noreply.github.com>
2026-08-12 11:12:16 +0800

    v2.1hotfix: APFSDS弹道属性硬编码绕过JSON加载 - 穿深250/韧性120/跳弹0.03. 解决被木板单防问题.

--- src/main/java/com/cbcaddon/addon/entity/APFSDSAutocannonProjectile.java
+++ src/main/java/com/cbcaddon/addon/entity/APFSDSAutocannonProjectile.java
@@ -3,9 +3,19 @@ package com.cbcaddon.addon.entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 import rbasamoyai.createbigcannons.munitions.autocannon.ap_round.APAutocannonProjectile;
+import rbasamoyai.createbigcannons.munitions.config.components.BallisticPropertiesComponent;
+import rbasamoyai.createbigcannons.munitions.config.components.EntityDamagePropertiesComponent;
 
 public class APFSDSAutocannonProjectile extends APAutocannonProjectile {
     private boolean highVelocity = false;
+
+    private static final BallisticPropertiesComponent BALLISTIC = new BallisticPropertiesComponent(
+        -0.005, 0.001, false, 10.0f, 250.0f, 120.0f, 0.03f
+    );
+
+    private static final EntityDamagePropertiesComponent DAMAGE = new EntityDamagePropertiesComponent(
+        40.0f, false, true, false, 2.0f
+    );
 
     public APFSDSAutocannonProjectile(EntityType<? extends APFSDSAutocannonProjectile> type, Level level) {
         super(type, level);
@@ -13,6 +23,16 @@ public class APFSDSAutocannonProjectile extends APAutocannonProjectile {
 
     public void setHighVelocity(boolean hv) {
         this.highVelocity = hv;
+    }
+
+    @Override
+    protected BallisticPropertiesComponent getBallisticProperties() {
+        return BALLISTIC;
+    }
+
+    @Override
+    public EntityDamagePropertiesComponent getDamageProperties() {
+        return DAMAGE;
     }
 
     @Override
