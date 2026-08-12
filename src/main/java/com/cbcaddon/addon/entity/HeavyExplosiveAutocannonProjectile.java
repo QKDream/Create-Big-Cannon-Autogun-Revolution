35c35dc41c3a2107fe99a61a69922c6c76ca7d0d
QKDream <qkdream@users.noreply.github.com>
2026-08-13 00:37:18 +0800

    v2.2hotfix1: fix Heavy Explosive Round no-explosion (detonate on impact, bypass TB pass-through), add built-in flame/smoke trail

--- src/main/java/com/cbcaddon/addon/entity/HeavyExplosiveAutocannonProjectile.java
+++ src/main/java/com/cbcaddon/addon/entity/HeavyExplosiveAutocannonProjectile.java
@@ -1,14 +1,20 @@
 package com.cbcaddon.addon.entity;
 
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import rbasamoyai.createbigcannons.munitions.AbstractCannonProjectile.ImpactResult;
+import rbasamoyai.createbigcannons.munitions.ProjectileContext;
 import rbasamoyai.createbigcannons.munitions.autocannon.flak.FlakAutocannonProjectile;
 
 public class HeavyExplosiveAutocannonProjectile extends FlakAutocannonProjectile {
     private static final double MAX_SPEED = 5.0;
     private static final double MAX_SPEED_SQ = MAX_SPEED * MAX_SPEED;
     private static final double ACCELERATION = 1.04;
+    private boolean hasDetonated;
 
     public HeavyExplosiveAutocannonProjectile(EntityType<? extends HeavyExplosiveAutocannonProjectile> type, Level level) {
         super(type, level);
@@ -26,5 +32,21 @@ public class HeavyExplosiveAutocannonProjectile extends FlakAutocannonProjectile
             this.setDeltaMovement(accelerated);
         }
         super.tick();
+        if (!this.isRemoved() && !this.level().isClientSide && !this.isInGround()) {
+            Vec3 pos = this.position().subtract(this.getDeltaMovement().scale(0.4));
+            ((ServerLevel) this.level()).sendParticles(ParticleTypes.SMOKE, pos.x, pos.y, pos.z, 1, 0.0, 0.0, 0.0, 0.01);
+            ((ServerLevel) this.level()).sendParticles(ParticleTypes.FLAME, pos.x, pos.y, pos.z, 1, 0.0, 0.0, 0.0, 0.005);
+        }
+    }
+
+    @Override
+    protected boolean onImpact(HitResult hitResult, ImpactResult impactResult, ProjectileContext projectileContext) {
+        super.onImpact(hitResult, impactResult, projectileContext);
+        if (!this.level().isClientSide && !this.hasDetonated) {
+            this.hasDetonated = true;
+            this.detonate(hitResult.getLocation());
+            this.removeNextTick = true;
+        }
+        return true;
     }
 }
